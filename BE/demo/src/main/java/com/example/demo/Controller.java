b2bf6f59e16daf8c2c94a58ad5e3521889bd62e8
HuynhTienNhat <huynhngoctiennhat@gmail.com>
2025-03-04 12:58:40 +0700

    Add RestController

--- BE/demo/src/main/java/com/example/demo/Controller.java
+++ BE/demo/src/main/java/com/example/demo/Controller.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @RestController
@@ -12,4 +15,10 @@ public class Controller {
     public String test() {
         return "Hello, World!";
     }
+
+    @GetMapping("/{id}")
+    public String getMethodName(@PathVariable int id) {
+        return "Hello from id";
+    }
+    
 }
